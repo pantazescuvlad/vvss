c9c197edc204790016a3711bc21dbbc69dedc49a
Vlad Pantazescu <pantazescu.vlad@gmail.com>
2020-04-02 23:06:33 +0300

    added setUp for TasksOperationsTest"

--- Tasks/src/test/java/tasks/model/TasksOperationsTest.java
+++ Tasks/src/test/java/tasks/model/TasksOperationsTest.java
@@ -1,10 +1,31 @@
 package tasks.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import tasks.services.DateService;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TasksOperationsTest {
 
+    TasksOperations tasksOperations;
+
+    @BeforeAll
+    void setUp() {
+        List<Task> tasks = new ArrayList<>();
+        ObservableList<Task> tasksList = FXCollections.observableArrayList(tasks);
+        tasksOperations = new TasksOperations(tasksList);
+    }
+
+    @Test
+    void incoming_nextTime_is_null(){
+
+    }
 
 }
