d7e6dd4e37cc028d559888eaee31206b5e5eecf6
Vlad Pantazescu <pantazescu.vlad@gmail.com>
2020-04-25 21:09:57 +0300

    added ArrayTaskList tests

--- src/test/java/tasks/repository/ArrayTaskListTest.java
+++ src/test/java/tasks/repository/ArrayTaskListTest.java
@@ -1,12 +1,38 @@
 package tasks.repository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import tasks.model.Task;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ArrayTaskListTest {
 
+    ArrayTaskList arrayTaskList;
+
     @BeforeEach
     void setUp() {
+        arrayTaskList = new ArrayTaskList();
+    }
+
+    @Test
+    void add_task_null(){
+        Exception exception = assertThrows(NullPointerException.class, () -> {
+            arrayTaskList.add(null);
+        });
+
+        String expectedMessage = "Task shouldn't be null";
+        String actualMessage = exception.getMessage();
+
+        assert (actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void add_task_success(){
+        Task t = mock(Task.class);
+        arrayTaskList.add(t);
+
+        assert(arrayTaskList.getTask(0) == t);
     }
 }
